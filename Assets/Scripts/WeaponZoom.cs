5a1fd4e4c3e3b80321e8ca408516bac6fda5fe9a
kprasad1221 <kprasad.1221@gmail.com>
2020-04-18 10:40:21 -0800

    implement weapon zoom logic

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class WeaponZoom : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Camera cameraView;
+    [SerializeField] float zoomInFOV = 30f;
+    [SerializeField] float zoomOutFOV = 60f;
+    bool isZoomedIn = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (isZoomedIn == false)
+            {
+                cameraView.fieldOfView = zoomInFOV;
+                isZoomedIn = true;
+            }
+            else
+            {
+                cameraView.fieldOfView = zoomOutFOV;
+                isZoomedIn = false;
+            }
+            
+        }
     }
 }
